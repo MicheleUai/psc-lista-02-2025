6b273e1b909c67695a78c114d8b207da41bef51a
Michele Carvalho <michelecarvalho.uai@gmail.com>
2025-03-30 18:57:41 -0300

    Add files via upload

---
+++ notasbimestralaluno.java
@@ -0,0 +1,30 @@
+package DANIEL;
+ 
+import java.util.Scanner;
+
+public class notasbimestralaluno {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+
+        double nota1,nota2,nota3,nota4,media;
+
+        System.out.println("Digite a primeira nota: ");
+        nota1 = scanner.nextDouble();
+
+        System.out.println("Digite a segunda nota: ");
+        nota2 = scanner.nextDouble();
+
+        System.out.println("Digite a terceira nota: ");
+        nota3 = scanner.nextDouble();
+
+        System.out.println("Digite a quarta nota: ");
+        nota4 = scanner.nextDouble();
+
+        media = (nota1 + nota2 + nota3 + nota4) /4;
+
+        System.out.println("A média das 4 notas bimestrais é: " + media);
+
+        scanner.close();
+    }
+    
+}
